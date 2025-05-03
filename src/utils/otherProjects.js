10c344a5b2f71705706c4047706c8824edf8d5d3
yuvikakathaith <yuvikakathaith3103@gmail.com>
2025-05-03 18:37:13 +0530

    new project added

--- src/utils/otherProjects.js
+++ src/utils/otherProjects.js
@@ -1,93 +1,112 @@
 export const projectsData = [
-    {
-      id: 1,
-      title: "MERN Restaurant",
-      description: "A restaurant web app built using the MERN. This project showcases the use of MongoDB, Express, React, and Node.js to create a dynamic restaurant website. It allows users to interact with the backend to make reservations.",
-      github: "https://github.com/yuvikaKathaith/MERN-Restaurant",
-      live: "https://mern-restaurant-frontend-mocha.vercel.app/",
-      techs: ["MERN", "TheMealDB API"],
-      year: 2024
-    },
-    {
-      id: 2,
-      title: "AI Image Generator",
-      description: "Utilizing React and the OpenAI API, this project is an AI-powered image generator that creates images based on text prompts. A fun way to explore the capabilities of AI image generation.",
-      github: "https://github.com/yuvikaKathaith/AI-Image-app",
-      live: "https://ai-img-app.netlify.app/",
-      techs: ["React", "OpenAI API"],
-      year: 2024
-    },
-    {
-      id: 3,
-      title: "Gemini Clone",
-      description: "A clone of the Google Gemini AI chatbot powered by the Google Gemini API, built using React. Users can interact with the AI model, receiving responses in a conversational style.",
-      github: "https://github.com/yuvikaKathaith/Gemini-clone",
-      live: "https://gemini-clone-chi-rosy.vercel.app/",
-      techs: ["React", "CSS", "Google Gemini API"],
-      year: 2024
-    },
-    {
-      id: 4,
-      title: "Search Engine",
-      description: "A simple search engine built using HTML, CSS, and JavaScript. It mimics a basic search interface where you can input queries and see results displayed dynamically.",
-      github: "https://github.com/yuvikaKathaith/Search-engine",
-      live: "https://search-engine-finder.netlify.app/",
-      techs: ["HTML", "CSS", "JavaScript"],
-      year: 2023
-    },
-    {
-      id: 5,
-      title: "Page Animation",
-      description: "A webpage animation project demonstrating the use of GSAP for creating smooth animations that enhance user experience when navigating the page.",
-      github: "https://github.com/yuvikaKathaith/Page-Animation",
-      live: "https://pag-animation-gsap.netlify.app/",
-      techs: ["GSAP", "HTML", "CSS"],
-      year: 2024
-    },
-    {
-      id: 6,
-      title: "Currency Converter",
-      description: "A simple currency converter built using JS. This app allows users to convert one currency to another using real-time exchange rates fetched from the Exchangerate-API. It was a great project to practice working with APIs and handling dynamic data.",
-      github: "https://github.com/yuvikaKathaith/Currency-Converter",
-      live: "https://currency-converter-js-zeta.vercel.app/",
-      techs: ["HTML", "CSS", "JS", "Exchangerate-API"],
-      year: 2023
-    },
-    {
-      id: 7,
-      title: "Weather App",
-      description: "This was my first time using API integration, and I found it really enjoyable. The project fetches live weather data from an API and displays it in a simple, user-friendly interface. Now, I love working on projects where I get to use APIs!",
-      github: "https://github.com/yuvikaKathaith/Weather-App",
-      live: "https://weather-lcqd5hwwv-yuvika-kathaiths-projects.vercel.app/",
-      techs: ["HTML", "CSS", "JS", "OpenWeatherMap API"],
-      year: 2023
-    },
-    {
-      id: 8,
-      title: "Ecommerce Cart",
-      description: "A fully functional e-commerce cart showcasing dynamic product additions, real-time cart updates, and smooth interactions for a seamless online shopping experience.",
-      github: "https://github.com/yuvikaKathaith/EcommerceCart",
-      live: "https://ecommerce-cart-sigma.vercel.app/",
-      techs: ["HTML", "CSS", "JavaScript"],
-      year: 2023
-    },
-    {
-      id: 9,
-      title: "Movie Seat Booking App",
-      description: "A seat booking system where users can select their seats and make bookings for movies. This project is built with pure JavaScript, offering real-time seat availability and booking.",
-      github: "https://github.com/yuvikaKathaith/Movie-Seat-Booking-App",
-      live: "https://movie-seat-booking-app-tan.vercel.app/",
-      techs: ["HTML", "CSS", "JavaScript"],
-      year: 2022
-    },
-    {
-      id: 10,
-      title: "Simon Game",
-      description: "This was the very first project that sparked my interest in web development. Inspired by the Simon game I played as a kid, I recreated it using HTML, CSS, and JavaScript by following tutorials. It was tough but super rewarding!",
-      github: "https://github.com/yuvikaKathaith/Simon-Game",
-      live: "https://simon-game-j.netlify.app/",
-      techs: ["HTML", "CSS", "JavaScript"],
-      year: 2022
-    }
-  ];
-  
+  {
+    id: 1,
+    title: "Advanced Pokémon Explorer",
+    description:
+      "A sleek SPA built with React that lets users explore a wide range of Pokémon with features like type-based filtering, search, pagination, sorting, detailed Pokémon pages and mark favourites.",
+    github: "https://github.com/yuvikaKathaith/poke-explorer",
+    live: "https://poke-explorer-rho.vercel.app/", 
+    techs: ["React", "Tailwind", "PokéAPI", "React Router", "Context API"],
+    year: 2025,
+  },
+  {
+    id: 2,
+    title: "MERN Restaurant",
+    description:
+      "A restaurant web app built using the MERN. This project showcases the use of MongoDB, Express, React, and Node.js to create a dynamic restaurant website. It allows users to interact with the backend to make reservations.",
+    github: "https://github.com/yuvikaKathaith/MERN-Restaurant",
+    live: "https://mern-restaurant-frontend-mocha.vercel.app/",
+    techs: ["MERN", "TheMealDB API"],
+    year: 2024,
+  },
+  {
+    id: 3,
+    title: "AI Image Generator",
+    description:
+      "Utilizing React and the OpenAI API, this project is an AI-powered image generator that creates images based on text prompts. A fun way to explore the capabilities of AI image generation.",
+    github: "https://github.com/yuvikaKathaith/AI-Image-app",
+    live: "https://ai-img-app.netlify.app/",
+    techs: ["React", "OpenAI API"],
+    year: 2024,
+  },
+  {
+    id: 4,
+    title: "Gemini Clone",
+    description:
+      "A clone of the Google Gemini AI chatbot powered by the Google Gemini API, built using React. Users can interact with the AI model, receiving responses in a conversational style.",
+    github: "https://github.com/yuvikaKathaith/Gemini-clone",
+    live: "https://gemini-clone-chi-rosy.vercel.app/",
+    techs: ["React", "CSS", "Google Gemini API"],
+    year: 2024,
+  },
+  {
+    id: 5,
+    title: "Search Engine",
+    description:
+      "A simple search engine built using HTML, CSS, and JavaScript. It mimics a basic search interface where you can input queries and see results displayed dynamically.",
+    github: "https://github.com/yuvikaKathaith/Search-engine",
+    live: "https://search-engine-finder.netlify.app/",
+    techs: ["HTML", "CSS", "JavaScript"],
+    year: 2023,
+  },
+  {
+    id: 6,
+    title: "Page Animation",
+    description:
+      "A webpage animation project demonstrating the use of GSAP for creating smooth animations that enhance user experience when navigating the page.",
+    github: "https://github.com/yuvikaKathaith/Page-Animation",
+    live: "https://pag-animation-gsap.netlify.app/",
+    techs: ["GSAP", "HTML", "CSS"],
+    year: 2024,
+  },
+  {
+    id: 7,
+    title: "Currency Converter",
+    description:
+      "A simple currency converter built using JS. This app allows users to convert one currency to another using real-time exchange rates fetched from the Exchangerate-API. It was a great project to practice working with APIs and handling dynamic data.",
+    github: "https://github.com/yuvikaKathaith/Currency-Converter",
+    live: "https://currency-converter-js-zeta.vercel.app/",
+    techs: ["HTML", "CSS", "JS", "Exchangerate-API"],
+    year: 2023,
+  },
+  {
+    id: 8,
+    title: "Weather App",
+    description:
+      "This was my first time using API integration, and I found it really enjoyable. The project fetches live weather data from an API and displays it in a simple, user-friendly interface. Now, I love working on projects where I get to use APIs!",
+    github: "https://github.com/yuvikaKathaith/Weather-App",
+    live: "https://weather-lcqd5hwwv-yuvika-kathaiths-projects.vercel.app/",
+    techs: ["HTML", "CSS", "JS", "OpenWeatherMap API"],
+    year: 2023,
+  },
+  {
+    id: 9,
+    title: "Ecommerce Cart",
+    description:
+      "A fully functional e-commerce cart showcasing dynamic product additions, real-time cart updates, and smooth interactions for a seamless online shopping experience.",
+    github: "https://github.com/yuvikaKathaith/EcommerceCart",
+    live: "https://ecommerce-cart-sigma.vercel.app/",
+    techs: ["HTML", "CSS", "JavaScript"],
+    year: 2023,
+  },
+  {
+    id: 10,
+    title: "Movie Seat Booking App",
+    description:
+      "A seat booking system where users can select their seats and make bookings for movies. This project is built with pure JavaScript, offering real-time seat availability and booking.",
+    github: "https://github.com/yuvikaKathaith/Movie-Seat-Booking-App",
+    live: "https://movie-seat-booking-app-tan.vercel.app/",
+    techs: ["HTML", "CSS", "JavaScript"],
+    year: 2022,
+  },
+  {
+    id: 11,
+    title: "Simon Game",
+    description:
+      "This was the very first project that sparked my interest in web development. Inspired by the Simon game I played as a kid, I recreated it using HTML, CSS, and JavaScript by following tutorials. It was tough but super rewarding!",
+    github: "https://github.com/yuvikaKathaith/Simon-Game",
+    live: "https://simon-game-j.netlify.app/",
+    techs: ["HTML", "CSS", "JavaScript"],
+    year: 2022,
+  },
+];
